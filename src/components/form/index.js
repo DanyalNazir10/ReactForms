60332812f9786fd29d1b11cc7d6c018847519ebd
DanyalNazir10 <danyal.nazir@mergestack.com>
2023-11-14 00:31:09 -0800

    Managed from states

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,33 +1,40 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
-const form = () => {
+const Form = () => {
+
+    const {register, control} = useForm();
 
     return(
+        <>
             <form>
                 <label htmlFor="firstName">First Name</label>
-                <input type="text" id="firstName" name="firstName" placeholder="Enter your first name"/>
+                <input type="text" id="firstName" {...register("firstName")} placeholder="Enter your first name"/>
 
                 <label htmlFor="lastName">Last Name</label>
-                <input type="text" id="lastName" name="lastName" placeholder="Enter your last name"/>
+                <input type="text" id="lastName" {...register("lastName")} placeholder="Enter your last name"/>
                 
                 <label htmlFor="age">Age</label>
-                <input type="number" id="age" name="age" placeholder="Enter your age"/>
+                <input type="number" id="age" {...register("age")} placeholder="Enter your age"/>
                 
                 <label htmlFor="email">Notification Emails</label>
-                <input type="email" id="email" name="email" placeholder="Enter your emails"/>
+                <input type="email" id="email" {...register("email")} placeholder="Enter your emails"/>
 
                 <label htmlFor="contact">Contact Number</label>
-                <input type="tel" id="contact" name="contact" placeholder="Enter your contact number"/>
+                <input type="tel" id="contact" {...register("contact")} placeholder="Enter your contact number"/>
 
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Enter your password"/>
+                <input type="password" id="password" {...register("password")} placeholder="Enter your password"/>
 
                 <label htmlFor="confirmPassword">Confirm Password</label>
-                <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm your password"/>
+                <input type="password" id="confirmPassword" {...register("confirmPassword")} placeholder="Confirm your password"/>
 
                 <input type="submit" value={"Submit"}/>
             </form>
+            <DevTool control={control}/> 
+        </>
     )
 }
 
-export default form;
+export default Form;
